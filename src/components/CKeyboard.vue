9f9fa7199c8641a3ffb2e9d0c44a045b2872bb18
vladkuraev84 <vladkuraev84@gmail.com>
2021-04-21 00:03:21 +0300

    worked with roulette 2

--- src/components/CKeyboard.vue
+++ src/components/CKeyboard.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="c-keyboard-wr">
+    <p class="choice">{{ choice }}</p>
     <div class="c-keyboard">
       <button
         name="19 to 36"
         class="c-btn c-range-1936"
         id="range1936"
-        data-amount="">
+        @click="active"
+        :data-amount="range1936">
           <span class="">
             <span class="">19 to 36</span>
           </span>
@@ -17,7 +19,8 @@
         name="odd"
         class="c-btn c-odd"
         id="odd"
-        data-amount="">
+        @click="active"
+        :data-amount="odd">
           <span class="">
             <span class="">Odd</span>
           </span>
@@ -29,7 +32,8 @@
         name="black"
         class="c-btn c-black"
         id="black"
-        data-amount="">
+        @click="active"
+        :data-amount="black">
           <span class="">
             <span class="">Black</span>
           </span>
@@ -41,7 +45,8 @@
         name="red"
         class="c-btn c-red"
         id="red"
-        data-amount="">
+        @click="active"
+        :data-amount="red">
           <span class="">
             <span class="">Red</span>
           </span>
@@ -53,7 +58,8 @@
         name="even"
         class="c-btn c-even"
         id="even"
-        data-amount="">
+        @click="active"
+        :data-amount="even">
           <span class="">
             <span class="">Even</span>
           </span>
@@ -65,7 +71,8 @@
         name="1 to 18"
         class="c-btn c-range-118"
         id="range118"
-        data-amount="">
+        @click="active"
+        :data-amount="range118">
           <span class="">
             <span class="">1 to 18</span>
           </span>
@@ -77,7 +84,8 @@
         name="25 to 36"
         class="c-btn c-range-2536"
         id="range2536"
-        data-amount="">
+        @click="active"
+        :data-amount="range2536">
           <span class="">
             <span class="">25 to 36</span>
           </span>
@@ -89,7 +97,8 @@
         name="13 to 24"
         class="c-btn c-range-1324"
         id="range1324"
-        data-amount="">
+        @click="active"
+        :data-amount="range1324">
           <span class="">
             <span class="">13 to 24</span>
           </span>
@@ -101,7 +110,8 @@
         name="1 to 12"
         class="c-btn c-range-112"
         id="range112"
-        data-amount="">
+        @click="active"
+        :data-amount="range112">
           <span class="">
             <span class="">1 to 12</span>
           </span>
@@ -113,7 +123,8 @@
         name="1:1"
         class="c-btn c-row1"
         id="row1"
-        data-amount="">
+        @click="active"
+        :data-amount="row1">
           <span class="">
             <span class="">1:1</span>
           </span>
@@ -125,7 +136,8 @@
         name="2:1"
         class="c-btn c-row2"
         id="row2"
-        data-amount="">
+        @click="active"
+        :data-amount="row2">
           <span class="">
             <span class="">2:1</span>
           </span>
@@ -137,7 +149,8 @@
         name="3:1"
         class="c-btn c-row3"
         id="row3"
-        data-amount="">
+        @click="e => e.target.classList.toggle('active')"
+        :data-amount="row3">
           <span class="">
             <span class="">3:1</span>
           </span>
@@ -151,6 +164,7 @@
         :class="`color-simple c-btn number-${index}`"
         :id="`colorSimple-${index}`"
         :data-amount="`${index}`"
+        @click="active"
       >
           <span class="">
             <span class="">{{ index }}</span>
@@ -167,8 +181,39 @@
   export default {
     data(){
       return {
-        items: 37
+        items: 37,
+        choice: 'Сделайте свой выбор',
+        range1936: [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36],
+        odd: [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35],
+        black: [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,35],
+        red: [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,33,34,36],
+        even: [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36],
+        range118: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        range2536: [25,26,27,28,29,30,31,32,33,34,35,36],
+        range1324: [13,14,15,16,17,18,19,20,21,22,23,24],
+        range112: [1,2,3,4,5,6,7,8,9,10,11,12],
+        row1: [3,6,9,12,15,18,21,24,27,30,33,36],
+        row2: [2,5,8,11,14,17,20,23,26,29,32,35],
+        row3: [1,4,7,10,13,16,19,22,25,28,31,34],
       }
     },
+    methods: {
+      active(e) {
+        this.choice = 'Вы выбрали: ';
+
+        e.target.classList.toggle('active');
+        let elements = document.querySelectorAll('.c-btn.active');
+
+        for (let elem of elements) {
+          let res = null;
+          res = elem.dataset.amount;
+          this.choice += res.concat(', ');
+        }
+
+        if (elements.length == 0) {
+          this.choice = 'Вы ничего не выбрали'
+        }
+      }
+    }
   }
 </script>
--- src/components/CRoulette.vue
+++ src/components/CRoulette.vue
@@ -2,8 +2,17 @@
   <div class="c-roulette">
     <div class="c-roulette">
       <div class="c-roulette-ar"><img src="../assets/img/roulette-ar.svg" alt="arrow"></div>
-      <canvas ref="ccanvas" width="338" height="338" :style="{ 'transform': degree ? 'none' : null }" />
+      <canvas
+          ref="ccanvas"
+          width="338"
+          height="338"
+          :style="{'transform': 'rotate(' + degree + 'deg)'}"
+      />
+      <span class="number" ref="position">{{ position }}</span>
+      <!--          :style="{ 'transform': degree ? 'none' : null }" -->
     </div>
+    <br/>
+    <br/>
     <button @click="startRoulette">Roll!</button>
   </div>
 </template>
@@ -13,46 +22,49 @@ export default {
   name: "roulette",
   data() {
     return {
-      degree: null,
+      position: null,
+      random: '',
+      degree: 0,
+      total: 0,
       timeout: null,
       options: [
-          '36',
-          '11',
-          '30',
-          '8',
-          '23',
-          '10',
-          '5',
-          '24',
-          '16',
-          '33',
-          '1',
-          '20',
-          '14',
-          '31',
-          '9',
-          '22',
-          '18',
-          '29',
-          '7',
-          '28',
-          '12',
-          '35',
-          '3',
-          '26',
-          '0',
-          '32',
-          '15',
-          '19',
-          '4',
-          '21',
-          '2',
-          '25',
-          '17',
-          '34',
-          '6',
-          '27',
-          '13'
+          36,
+          11,
+          30,
+          8,
+          23,
+          10,
+          5,
+          24,
+          16,
+          33,
+          1,
+          20,
+          14,
+          31,
+          9,
+          22,
+          18,
+          29,
+          7,
+          28,
+          12,
+          35,
+          3,
+          26,
+          0,
+          32,
+          15,
+          19,
+          4,
+          21,
+          2,
+          25,
+          17,
+          34,
+          6,
+          27,
+          13
       ],
     }
   },
@@ -62,14 +74,16 @@ export default {
 
   methods: {
     startRoulette() {
-      console.log(this.$refs.ccanvas);
+      this.position = '';
 
-      let random = Math.floor(Math.random() * 36);
-      // this.$refs.ccanvas.style.transform = 'rotate(360deg)';
-      console.log(random);
-      this.timeout = setInterval(() => {
-        this.$refs.ccanvas.style.transform = 'rotate(360deg)';
-      }, 1)
+      this.random = Math.floor(Math.random() * 36);
+      let positionNum = this.options.indexOf(this.random);
+
+      this.degree = Number(360 + (360 - positionNum*10) + 5);
+
+      setInterval(() => {
+        this.position = this.random
+      }, 7200)
 
     }
   }
